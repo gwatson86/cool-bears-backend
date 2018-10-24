cb95bbee9e9b7e2fca10bc18c95c9afa781a333d
Kim Pham <pham.kim.a@gmail.com>
2018-10-24 09:10:48 -0500

    Add more bear seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Hooman.create(
 )
 
 Bear.create(
-    name: "Brownie",
+    name: "Michael",
     species: "Brown Bear",
     nationality: "American",
     image_url: "https://i.dailymail.co.uk/i/pix/2017/07/21/15/42885FD900000578-0-image-a-29_1500647282323.jpg",
@@ -22,6 +22,60 @@ Bear.create(
     species: "Polar Bear",
     nationality: "Russian",
     image_url: "https://secure.i.telegraph.co.uk/multimedia/archive/01559/wavingbear_1559453c.jpg",
+    hooman_id: 1,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Lee",
+    species: "Asiatic Black Bear",
+    nationality: "Korean",
+    image_url: "https://cdnph.upi.com/svc/sv/i/4111472840822/2016/1/14728422879510/Japanese-fisherman-uses-karate-skills-to-fend-off-bear.jpg",
+    hooman_id: 2,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Noah",
+    species: "American Black Bear",
+    nationality: "Canadian",
+    image_url: "https://images.fineartamerica.com/images-medium-large/black-bear-waving-carolyn-fox.jpg",
+    hooman_id: 2,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Nguyen",
+    species: "Sun Bear",
+    nationality: "Vietnamese",
+    image_url: "https://photos.smugmug.com/Animals/Free-the-Bears/i-grcfrgw/0/ab284f24/L/7%20koh-IMG_1393-L.jpg",
+    hooman_id: 2,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Fathima",
+    species: "Sloth Bear",
+    nationality: "Sri Lankan",
+    image_url: "http://i.imgur.com/sy0izel.jpg",
+    hooman_id: 2,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Daniela",
+    species: "Spectacled Bear",
+    nationality: "Ecuadorian",
+    image_url: "https://images3.alphacoders.com/673/673734.jpg",
+    hooman_id: 2,
+    top_time: 100000000
+)
+
+Bear.create(
+    name: "Lily",
+    species: "Giant Panda",
+    nationality: "Chinese",
+    image_url: "https://www.telegraph.co.uk/content/dam/news/2016/08/23/106598324PandawaveNEWS_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=450",
     hooman_id: 2,
     top_time: 100000000
 )
