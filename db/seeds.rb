fa178ec8b3a6d629267dfb0117ab49cfdaf0f4e0
gwatson86 <gwatson86@gmail.com>
2018-10-25 10:14:06 -0500

    finished all image sources

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@ Bear.create(
     name: "Michael",
     species: "Brown Bear",
     nationality: "American",
-    image_url: "https://i.dailymail.co.uk/i/pix/2017/07/21/15/42885FD900000578-0-image-a-29_1500647282323.jpg",
+    image_url: "brown-bear.jpg",
+    eaten_image: "eaten-brown-bear.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -21,7 +22,8 @@ Bear.create(
     name: "Boris",
     species: "Polar Bear",
     nationality: "Russian",
-    image_url: "https://secure.i.telegraph.co.uk/multimedia/archive/01559/wavingbear_1559453c.jpg",
+    image_url: "polar-bear.jpg",
+    eaten_image: "eaten-polar-bear.jpg",
     hooman_id: 1,
     top_time: 100000000
 )
@@ -30,7 +32,8 @@ Bear.create(
     name: "Lee",
     species: "Asiatic Black Bear",
     nationality: "Korean",
-    image_url: "https://cdnph.upi.com/svc/sv/i/4111472840822/2016/1/14728422879510/Japanese-fisherman-uses-karate-skills-to-fend-off-bear.jpg",
+    image_url: "asiatic-black-bear.jpg",
+    eaten_image: "eaten-asiatic-black-bear.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -39,7 +42,8 @@ Bear.create(
     name: "Noah",
     species: "American Black Bear",
     nationality: "Canadian",
-    image_url: "https://images.fineartamerica.com/images-medium-large/black-bear-waving-carolyn-fox.jpg",
+    image_url: "american-black-bear.jpg",
+    eaten_image: "eaten-american-black-bear.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -48,7 +52,8 @@ Bear.create(
     name: "Nguyen",
     species: "Sun Bear",
     nationality: "Vietnamese",
-    image_url: "https://photos.smugmug.com/Animals/Free-the-Bears/i-grcfrgw/0/ab284f24/L/7%20koh-IMG_1393-L.jpg",
+    image_url: "sun-bear.jpg",
+    eaten_image: "eaten-sun-bear-2.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -57,7 +62,8 @@ Bear.create(
     name: "Fathima",
     species: "Sloth Bear",
     nationality: "Sri Lankan",
-    image_url: "http://i.imgur.com/sy0izel.jpg",
+    image_url: "sloth-bear.jpg",
+    eaten_image: "eaten-sloth-bear.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -66,7 +72,8 @@ Bear.create(
     name: "Daniela",
     species: "Spectacled Bear",
     nationality: "Ecuadorian",
-    image_url: "https://images3.alphacoders.com/673/673734.jpg",
+    image_url: "spectacled-bear.jpg",
+    eaten_image: "eaten-spectacled-bear.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
@@ -75,7 +82,8 @@ Bear.create(
     name: "Lily",
     species: "Giant Panda",
     nationality: "Chinese",
-    image_url: "https://www.telegraph.co.uk/content/dam/news/2016/08/23/106598324PandawaveNEWS_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=450",
+    image_url: "asshole-bear.webp",
+    eaten_image: "eaten-panda.jpg",
     hooman_id: 2,
     top_time: 100000000
 )
